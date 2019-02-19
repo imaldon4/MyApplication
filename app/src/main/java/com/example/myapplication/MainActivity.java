37a2b2d2f485cf983a8b20cabc82c33fa49434ad
ike <imaldon4@mail.ccsf.edu>
2019-02-19 13:33:34 -0800

    Ini

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,PictureGalleryActivity.class);
+
+    }
+
+    public void toGallery(View view) {
+    }
+}
